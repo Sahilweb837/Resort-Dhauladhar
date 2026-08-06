91a6faa26eaec02154623841bc79a039622f1630
Sahilweb837 <sahilsandhu39234@gmail.com>
2026-08-06 12:02:28 +0530

    newcode

--- admin/blogs/create.php
+++ admin/blogs/create.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/../../includes/functions.php';
 requireLogin();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $error = '';
 $success = '';
